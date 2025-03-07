f1e175abb1849f3d74b5579020bc6e5b52a7ca9f
28acharyau <28acharyau@bwschools.net>
2025-03-07 11:23:08 -0500

    commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import turtle
+screen = turtle.Screen()
+screen.screensize(1000,1000)
+screen.bgcolor("tan")
+
+t = turtle.Turtle()
+#blue circle
+t.penup()
+t.goto(-80,15)
+t.pendown()
+t.pencolor("blue")
+t.circle(35)
+
+#black circle
+t.penup()
+t.goto(0,15)
+t.pendown()
+t.pencolor("black")
+t.circle(35)
+
+#yellow circle
+t.penup()
+t.goto(-40,-15)
+t.pendown()
+t.pencolor("yellow")
+t.circle(35)
+
+#green cricle
+t.penup()
+t.goto(40,-15)
+t.pendown()
+t.pencolor("green")
+t.circle(35)
+
+#red circle
+t.penup()
+t.goto(80,15)
+t.pendown()
+t.pencolor("red")
+t.circle(35)
+t.penup()
+
+#text1
+t.goto(0,100)
+t.pencolor("purple")
+t.write("Winter Olympics" , font=("Arial",30,"bold italic"),align='center')
+
+t.goto(0,-100)
+t.write("2026" , font=("Arial",30,"bold italic"),align='center')
+
+turtle.done()
+
+
+
+
+
+
+
